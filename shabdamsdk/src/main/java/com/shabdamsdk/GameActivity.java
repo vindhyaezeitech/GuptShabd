26b13381fbb83fcfd53751e3b999280be5a4fe77
ezeiatechanil <anil.kumar@ezeiatech.in>
2022-03-31 10:53:21 +0530

    bug fixed

--- shabdamsdk/src/main/java/com/shabdamsdk/GameActivity.java
+++ shabdamsdk/src/main/java/com/shabdamsdk/GameActivity.java
@@ -195,8 +195,6 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                         //Log.i(TAG, loadAdError.getMessage());
                         mInterstitialAd = null;
                     }
-
-
                 });
 
     }
